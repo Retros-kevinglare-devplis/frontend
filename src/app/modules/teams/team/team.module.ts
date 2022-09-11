dc2fa9cd955027826f5c930a90df7f3a767e77e8
pliskachev.e <pliskachev.e@medindex.ru>
2022-09-11 19:34:26 +0300

    Team rerender

--- src/app/modules/teams/team/team.module.ts
+++ src/app/modules/teams/team/team.module.ts
@@ -6,9 +6,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NewTeamComponent } from './new-team/new-team.component';
+import { AvatarModule } from '../../../shared/components/avatar/avatar.module';
 
 @NgModule({
   declarations: [TeamComponent, NewTeamComponent],
-  imports: [CommonModule, TeamRoutingModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    TeamRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    AvatarModule,
+  ],
 })
 export class TeamModule {}
